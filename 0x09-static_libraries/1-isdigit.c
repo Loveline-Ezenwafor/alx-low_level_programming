dc950fa9c4ab4ef77b8f063dca5c08aa3cb82fa3
Loveline-Ezenwafor <ezenwaforchioma@gmail.com>
2023-04-12 05:33:26 -0700

    task isd

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @w: int to be checked
+ * Return: 1 if w is a digit, 0 otherwise
+ */
+int _isdigit(int w)
+{
+	return (w >= '0' && w <= '9');
+}
